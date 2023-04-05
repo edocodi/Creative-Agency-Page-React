b562f9467836e5c3601a30ef7c722977d701c5cb
edocodi <youngercool@gmail.com>
2023-04-05 03:20:30 -0700

    Commit from TeleportHQ

--- src/views/about.js
+++ src/views/about.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { Helmet } from 'react-helmet'
 
 import Navigation from '../components/navigation'
+import GalleryCard3 from '../components/gallery-card3'
 import './about.css'
 
 const About = (props) => {
@@ -15,13 +16,92 @@ const About = (props) => {
       <div className="about-container1">
         <Navigation rootClassName="navigation-root-class-name"></Navigation>
       </div>
-      <div className="about-container2">
-        <div className="about-container3">
-          <img
-            src="https://images.unsplash.com/photo-1680499661732-3cfae4690e1c?ixid=Mnw5MTMyMXwwfDF8YWxsfDM3fHx8fHx8Mnx8MTY4MDY4OTczMA&amp;ixlib=rb-4.0.3&amp;w=400"
-            alt="image"
-            className="about-image"
-          />
+      <div className="about-gallery">
+        <h1 className="about-text">Our portfolio</h1>
+        <span className="about-text1">
+          <span>
+            <span>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
+              volutpat turpis.
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+            <span>
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+          </span>
+          <br></br>
+          <span>
+            <span>
+              Mauris luctus rutrum mi ut rhoncus. Integer in dignissim tortor.
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+            <span>
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+          </span>
+        </span>
+        <div className="about-container2">
+          <GalleryCard3 rootClassName="rootClassName"></GalleryCard3>
+          <GalleryCard3
+            image_src="https://images.unsplash.com/photo-1501127122-f385ca6ddd9d?ixid=Mnw5MTMyMXwwfDF8c2VhcmNofDExfHxtaW5pbWFsaXNtJTIwaG9tZXxlbnwwfHx8fDE2MjY0NDg1Mjc&amp;ixlib=rb-1.2.1&amp;h=1000"
+            rootClassName="rootClassName1"
+          ></GalleryCard3>
+          <GalleryCard3
+            image_src="https://images.unsplash.com/photo-1467043153537-a4fba2cd39ef?ixid=Mnw5MTMyMXwwfDF8c2VhcmNofDR8fG1pbmltYWxpc20lMjBob21lfGVufDB8fHx8MTYyNjQ0ODUyNw&amp;ixlib=rb-1.2.1&amp;h=1000"
+            rootClassName="rootClassName3"
+          ></GalleryCard3>
+          <GalleryCard3
+            image_src="https://images.unsplash.com/photo-1531829039722-d3fb3e705a4b?ixid=Mnw5MTMyMXwwfDF8c2VhcmNofDE4fHxtaW5pbWFsaXNtJTIwaG9tZXxlbnwwfHx8fDE2MjY0NDg1Mjc&amp;ixlib=rb-1.2.1&amp;h=1000"
+            rootClassName="rootClassName2"
+          ></GalleryCard3>
+          <GalleryCard3
+            image_src="https://images.unsplash.com/photo-1519710164239-da123dc03ef4?ixid=Mnw5MTMyMXwwfDF8c2VhcmNofDExfHxtaW5pbWFsaXNtfGVufDB8fHx8MTYyNjQ0NTY1Nw&amp;ixlib=rb-1.2.1&amp;h=1000"
+            rootClassName="rootClassName4"
+          ></GalleryCard3>
+          <GalleryCard3
+            image_src="https://images.unsplash.com/photo-1494438639946-1ebd1d20bf85?ixid=Mnw5MTMyMXwwfDF8c2VhcmNofDJ8fG1pbmltYWxpc218ZW58MHx8fHwxNjI2NDQ1NjU3&amp;ixlib=rb-1.2.1&amp;h=1000"
+            rootClassName="rootClassName5"
+          ></GalleryCard3>
+          <GalleryCard3
+            image_src="https://images.unsplash.com/photo-1504198458649-3128b932f49e?ixid=Mnw5MTMyMXwwfDF8c2VhcmNofDN8fG1pbmltYWxpc20lMjBob21lfGVufDB8fHx8MTYyNjQ0ODUyNw&amp;ixlib=rb-1.2.1&amp;h=1000"
+            rootClassName="rootClassName6"
+          ></GalleryCard3>
+          <GalleryCard3
+            image_src="https://images.unsplash.com/photo-1453904300235-0f2f60b15b5d?ixid=Mnw5MTMyMXwwfDF8c2VhcmNofDE3fHxtaW5pbWFsaXNtJTIwaG9tZXxlbnwwfHx8fDE2MjY0NDg1Mjc&amp;ixlib=rb-1.2.1&amp;h=1000"
+            rootClassName="rootClassName7"
+          ></GalleryCard3>
+          <GalleryCard3
+            image_src="https://images.unsplash.com/photo-1493552152660-f915ab47ae9d?ixid=Mnw5MTMyMXwwfDF8c2VhcmNofDd8fG1pbmltYWxpc20lMjBob21lfGVufDB8fHx8MTYyNjQ0ODUyNw&amp;ixlib=rb-1.2.1&amp;h=1000"
+            rootClassName="rootClassName8"
+          ></GalleryCard3>
+          <GalleryCard3
+            image_src="https://images.unsplash.com/photo-1510172951991-856a654063f9?ixid=Mnw5MTMyMXwwfDF8c2VhcmNofDIwfHxtaW5pbWFsaXNtJTIwaG9tZXxlbnwwfHx8fDE2MjY0NDg1Mjc&amp;ixlib=rb-1.2.1&amp;h=1000"
+            rootClassName="rootClassName9"
+          ></GalleryCard3>
+          <GalleryCard3
+            image_src="https://images.unsplash.com/photo-1449247709967-d4461a6a6103?ixid=Mnw5MTMyMXwwfDF8c2VhcmNofDN8fG1pbmltYWxpc218ZW58MHx8fHwxNjI2NDQ1NjU3&amp;ixlib=rb-1.2.1&amp;h=1000"
+            rootClassName="rootClassName10"
+          ></GalleryCard3>
+          <GalleryCard3
+            image_src="https://images.unsplash.com/photo-1472157510410-64a053cbc39f?ixid=Mnw5MTMyMXwwfDF8c2VhcmNofDh8fG1pbmltYWxpc20lMjBob21lfGVufDB8fHx8MTYyNjQ0ODUyNw&amp;ixlib=rb-1.2.1&amp;h=1000"
+            rootClassName="rootClassName11"
+          ></GalleryCard3>
         </div>
       </div>
     </div>
